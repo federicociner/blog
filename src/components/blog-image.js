89662333686723547e4e8e91567e5027610af24f
FedericoCiner <federico.ciner@gmail.com>
2020-03-31 11:27:43 +1100

    Finished NZ post

--- src/components/blog-image.js
+++ src/components/blog-image.js
@@ -7,21 +7,21 @@ const BlogImage = ({ path, title, alt }) => {
     textAlign: "center"
   };
 
-  const tinyImage = `${path}?nf_resize=fit&w=200 200w`;
-  const smallImage = `${path}?nf_resize=fit&w=400 400w`;
-  const mediumImage = `${path}?nf_resize=fit&w=800 800w`;
-  const largeImage = `${path}?nf_resize=fit&w=1200 1200w`;
-  const hugeImage = `${path}?nf_resize=fit&w=1600 1600w`;
+  const tinyImage = `${path}?nf_resize=fit&w=200`;
+  const smallImage = `${path}?nf_resize=fit&w=400`;
+  const mediumImage = `${path}?nf_resize=fit&w=800`;
+  const largeImage = `${path}?nf_resize=fit&w=1200`;
+  const hugeImage = `${path}?nf_resize=fit&w=1600`;
 
   return (
     <div>
       <picture>
-        <source media="(min-width: 1800px)" data-srcset={hugeImage} />
-        <source media="(max-width: 1300px)" data-srcset={largeImage} />
-        <source media="(max-width: 900px)" data-srcset={mediumImage} />
-        <source media="(max-width: 500px)" data-srcset={smallImage} />
         <source media="(max-width: 300px)" data-srcset={tinyImage} />
-        <img data-src={path} alt={alt} class="lazyload" />
+        <source media="(max-width: 500px)" data-srcset={smallImage} />
+        <source media="(max-width: 900px)" data-srcset={mediumImage} />
+        <source media="(max-width: 1300px)" data-srcset={largeImage} />
+        <source media="(min-width: 1800px)" data-srcset={hugeImage} />
+        <img data-src={path} alt={alt} className="lazyload" />
       </picture>
       <p style={captionStyle}>{title}</p>
     </div>
